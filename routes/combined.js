3526700011219fc5de5d322e5f93f20c3e78d6f5
james-muriithi <muriithijames556@gmail.com>
2020-03-29 21:20:10 +0300

    added index route

--- routes/combined.js
+++ routes/combined.js
@@ -1,13 +1,31 @@
-const get_data = require('../helpers/fetch-data');
+const express = require('express');
+const get_combined_data = require('../helpers/combined-data');
 
-const casesUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-const recoveredUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv';
-const deathsUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
+const router = express.Router();
 
+router.get('/', (req, res) => {
+    // fetch the data
+    get_combined_data()
+        .then(response => res.json(response));
+});
 
-Promise.all([get_data(casesUrl), get_data(recoveredUrl), get_data(deathsUrl)])
-    .then(result => {
-        console.log(result)
+router.get('/:country', (req, res) => {
+    // fetch the data
+    get_combined_data()
+        .then(response => {
 
-    })
-    .catch(error => console.log(`Error in promises ${error}`))
+            let countryData = {}
+                // loop through all the countries
+            response.forEach(country => {
+                    // for every country
+                    // if there is Country/Region is equal to passed country
+                    if (country['Country/Region'].toString().toLowerCase() === req.params.country.toLowerCase()) {
+                        countryData = country
+                    }
+                })
+                // send the country data back
+            res.send(countryData);
+        }).catch(e => console.log(e))
+});
+
+module.exports = router;
