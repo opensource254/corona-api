d19c098fde49deb46a60302cde892c8c4e3aebdd
james-muriithi <muriithijames556@gmail.com>
2020-03-29 18:26:55 +0300

    updated routing

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 
 const casesRoute = require('./routes/cases');
+const recoveredRoute = require('./routes/recovered');
+const deathsRoute = require('./routes/deaths')
 
 app.set('port', (process.env.PORT || 5000));
 
 // route index
-app.use('/', casesRoute);
-
+app.use('/reported', casesRoute);
+app.use('/recovered', recoveredRoute);
+app.use('/deaths', deathsRoute);
 app.listen(app.get('port'), () => {
     console.log(`Server started on port ${app.get('port')}`);
 });
