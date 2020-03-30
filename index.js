5741d460f8ed83ec2a9eb5a9270b27ed8d616a97
james-muriithi <muriithijames556@gmail.com>
2020-03-30 13:54:35 +0300

    edited routes

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const indexRoute = require('./routes/combined');
 app.set('port', (process.env.PORT || 5000));
 
 // route index
-app.use('/', indexRoute);
+
 app.use('/reported', casesRoute);
 app.use('/recovered', recoveredRoute);
 app.use('/deaths', deathsRoute);
+app.use('/', indexRoute);
 app.listen(app.get('port'), () => {
     console.log(`Server started on port ${app.get('port')}`);
 });
